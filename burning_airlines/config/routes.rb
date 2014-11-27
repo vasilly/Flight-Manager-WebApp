5177083c7c11cb4eb3a8d7b4f71cfad1a99a145d
Mattswann <mpdswann@gmail.com>
2014-11-27 16:48:53 +1100

    users files working

--- burning_airlines/config/routes.rb
+++ burning_airlines/config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+
+
   resources :aeroplanes
+
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
+
+
+  get '/sign_up' => 'users#new'
+  post '/sign_up' => 'users#create'
+
+  resources :users
+
+
 end
